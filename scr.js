19cf559dad279215362d8d9c0ca9766471326c0c
wecho12 <ehsankurd286@gmail.com>
2023-02-21 00:06:26 +0300

    done

---
+++ scr.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navlinks = document.querySelector(".navlinks");
+menuBtn.addEventListener("click", () => {
+  navlinks.classList.toggle("mobile-menu");
+});
